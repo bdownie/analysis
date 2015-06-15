1500c66abc821ddcb38110164a272def93392fb5
Bryan Downie <bryand@pobox.com>
2015-06-15 14:25:32 +0200

    Revised and made more general

--- makeReadPileup.R
+++ makeReadPileup.R
@@ -4,85 +4,89 @@ library(GenomicFeatures)
 library(GenomicAlignments)
 library(biovizBase)
 library(Rsamtools)
-data("genesymbol", package = "biovizBase")
+library(org.Hs.eg.db)
+library (TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(biomaRt)
 
+ensembl=useMart(biomart="ENSEMBL_MART_ENSEMBL",host="feb2014.archive.ensembl.org",path="/biomart/martservice",archive=FALSE)
+ensembl=useDataset("hsapiens_gene_ensembl",mart=ensembl)
 
-# Which genes to plot
-geneList <- c("TET3","TTC34","ADAMTS14")
-geneList <- c("KSR1P1")
-annotation_file <- "/ngsvol/Genomes/Homo_sapiens/ens37.75/Homo_sapiens.GRCh37.75.gtf"
-
-egrep.text <- paste(geneList,collapse="|")
-egrep.text <- paste("egrep \"",egrep.text,"\" ",annotation_file,sep = "")
-
-df.annotation <- read.table(pipe(egrep.text),sep="\t")
-
-names(df.annotation) <- c("chr","source","feature","start","end","score","strand","frame","attribute")
-df.annotation$gene_name = df.annotation$attribute
-df.annotation$gene_id = df.annotation$attribute
-df.annotation$exon_id = df.annotation$attribute
-df.annotation <- df.annotation[-9]
-
-df.annotation$gene_id <- sub(".*gene_id ","",df.annotation$gene_id,perl=TRUE)
-df.annotation$gene_id <- sub(";.*","",df.annotation$gene_id,perl=TRUE)
-
-df.annotation$gene_name <- sub(".*gene_name ","",df.annotation$gene_name,perl=TRUE)
-df.annotation$gene_name <- sub(";.*","",df.annotation$gene_name,perl=TRUE)
-
-df.annotation$exon_id <- sub(".*exon_id ","",df.annotation$exon_id,perl=TRUE)
-df.annotation$exon_id <- sub(";.*","",df.annotation$exon_id,perl=TRUE)
+gene_ids <- c(
+	"ENSG00000186205",
+	"ENSG00000133636",
+	"ENSG00000105723",
+	"ENSG00000148408",
+	"ENSG00000131471",
+	"ENSG00000092421"
+	)
 
 
 
-txdb <- makeTranscriptDbFromBiomart(biomart="ENSEMBL_MART_ENSEMBL", dataset="hsapiens_gene_ensembl",host="useast.ensembl.org")
-#txdb <- makeTranscriptDbFromGFF(file=annotation_file,format="gtf")
-#library(TxDb.Hsapiens.UCSC.hg19.knownGene)
-#txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
-#makeTranscriptDbFromBiomart
-# From http://davetang.org/muse/2013/01/02/iranges-and-genomicranges/
-annotation.gene <- with(df.annotation[df.annotation$feature == "gene",], GRanges(chr, IRanges(start, end), strand, score, id=gene_id))
-annotation.exon <- with(df.annotation[df.annotation$feature == "exon",], GRanges(chr, IRanges(start, end), strand, score, id=exon_id))
+genes.annotation=getBM(attributes=c("ensembl_gene_id", "ensembl_transcript_id","external_gene_id"),filters=c("ensembl_gene_id"),values=gene_ids, mart= ensembl) # fuction to get  gene id's and gene name from data base
+transcripts = genes.annotation$ensembl_transcript_id
 
-##
-#
-
-# Get ideogram for hg19
-#p.ideo <- Ideogram(genome = "hg19")
-
-# Limit
-#p.ideo + xlim(GRanges("chr2", IRanges(1e8, 1e8+10000000)))
-
-bfl <- c("bam/Sample_r827sCtr1_hs_chip_sr_Zeisberg_C_1.bam","bam/Sample_r827sCtr2_hs_chip_sr_Zeisberg_C_2.bam","bam/Sample_r827sTreat1_hs_chip_sr_Zeisberg_B_1.ds.bam","bam/Sample_r827sTreat2_hs_chip_sr_Zeisberg_B_2.ds.bam")
-bfl <- BamFileList(bfl)
+genes2id <- unique(genes.annotation[,c(1,3)])
+rownames(genes2id) <- genes2id[,1] 
 
 
 
-#bamfile <- BamFile(file="/ngsdata/intern/141126_SN933_0172_AC5J5GACXX/Aligned/Zeisberg_827/bam/Sample_r827sCtr1_hs_chip_sr_Zeisberg_C_1.bam")
-#ctr1.bam <- BamFile(file="/ngsdata/intern/141126_SN933_0172_AC5J5GACXX/Aligned/Zeisberg_827/bam/Sample_r827sCtr1_hs_chip_sr_Zeisberg_C_1.bam")
-#wh <- keepSeqlevels(txdb, "chr1")
-#autoplot(ctr1.bam, chr = "chr21")
+txdb=makeTxDbFromBiomart(transcript_ids=transcripts,biomart="ENSEMBL_MART_ENSEMBL",host="feb2014.archive.ensembl.org")
 
-for (i in 1:length(geneList)) {
-	n <- geneList[i]
+genes <-        genes(txdb)
+seqlevels(genes) <- sapply(seqlevels(genes),function(x) { if (any(grep("^\\d",x))) { paste("chr",x,sep="") } else { x } })
+exons <-        exonsBy(txdb, "gene")
+
+#wh.genes <- genes(txdb,vals=list(gene_id=gene_ids))
+wh.transcripts <- transcriptsBy(txdb ,by="gene")
 
 
-	p <- ScanBamParam(which = genesymbol[n])
+bfl <- c(
+	"/ngsdata/intern/150409_SN933_0186_AC6TN4ACXX/Aligned/Boemecke_909/bam/DiseaseUntreated.bam",
+	"/ngsdata/intern/150409_SN933_0186_AC6TN4ACXX/Aligned/Boemecke_909/bam/DiseaseEpi.bam",
+	"/ngsdata/intern/150409_SN933_0186_AC6TN4ACXX/Aligned/Boemecke_909/bam/ControlUntreated.bam",
+	"/ngsdata/intern/150409_SN933_0186_AC6TN4ACXX/Aligned/Boemecke_909/bam/ControlEpi.bam"
+	)
+bfn <- c(
+	"Disease Untreated",
+	"Disease Epi",
+	"Control Untreated",
+	"Control Epi"
+	)
 
-	ga1 <- readGAlignmentsFromBam(bfl[[1]], param=p,use.names=TRUE)
-	ga2 <- readGAlignmentsFromBam(bfl[[2]], param=p,use.names=TRUE)
-	ga3 <- readGAlignmentsFromBam(bfl[[3]], param=p,use.names=TRUE)
-	ga4 <- readGAlignmentsFromBam(bfl[[4]], param=p,use.names=TRUE)
-	p1 <- autoplot(ga1, geom = "line", stat = "coverage")
-	p2 <- autoplot(ga2, geom = "line", stat = "coverage")
-	p3 <- autoplot(ga3, geom = "line", stat = "coverage")
-	p4 <- autoplot(ga4, geom = "line", stat = "coverage")
+# Normalize sizes
+norm <- c(
+	135765722,
+	134176076,
+	89563374,
+	85207016
+)
+norm <- min(norm)/norm
 
-	
+pdf("genes.pdf")
+for (i in 1:length(gene_ids)) {
+	n <- gene_ids[i]
 
-	wh = genesymbol[geneList[i]]
-	wh <- range(wh, ignore.strand = TRUE)
+	#p <- ScanBamParam(which = genesymbol[n])
+	p <- ScanBamParam(which = genes[n])
 
-	genes <- autoplot(Homo.sapiens, which = wh, gap.geom = "chevron")
+	wh <- wh.transcripts[n][[1]]
+	z <- tracks(autoplot(txdb, which = wh, gap.geom = "chevron"),xlim=reduce(ranges(wh)),scale.height=2)
+	seqlevels(wh) <- sapply(seqlevels(wh),function(x) { if (any(grep("^\\d",x))) { paste("chr",x,sep="") } else { x } })
+	for (j in 1:length(bfl)) {
+		ga <- readGAlignments(bfl[j], param=p,use.names=TRUE)
+		ga <- ga[sample(length(ga),length(ga) * norm[j])]
 
-	tracks(Control1=p1,Control2=p2,Treatment1=p3,Treatment2=p4,title=paste("Coverage of",n)) + ylim(0,100) + tracks(gene=genes)
+		ga2 <- ga[(start(ga) > start(reduce(ranges(wh)))) & (end(ga) < end(reduce(ranges(wh))))]
+
+		x <- tracks(autoplot(ga2,  stat = "coverage",ylab=genes2id[n,2]))
+		if (j == 1) { 
+			y <- x
+		} else { 
+			y <- y + x
+		}
+	}
+	print(y + z + theme_alignment()  )
 }
+dev.off()
+
+
